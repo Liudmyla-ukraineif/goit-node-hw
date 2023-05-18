280cedbf4c424d4f90965dc57ad50c4541517731
Liudmyla <103444345+Liudmyla-ukraineif@users.noreply.github.com>
2023-05-18 18:07:07 +0300

    FIXED:: change on console.table

--- index.js
+++ index.js
@@ -17,27 +17,27 @@ const invokeAction = async({ action, id, name, email, phone })=> {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-      console.log(allContacts);
+      console.table(allContacts);
       break;
 
     case "get":
       const oneContactId = await contacts.getContactById(id);
-      console.log(oneContactId);
+      console.table(oneContactId);
       break;
 
     case "add":
       const newContact = await contacts.addContact({name, email, phone});
-      console.log(newContact);
+      console.table(newContact);
       break;
 
     case "update":
       const updateContact = await contacts.updateContact(id, {name, email, phone});
-      console.log(updateContact);
+      console.table(updateContact);
       break;
 
     case "remove":
       const removeContact = await contacts.removeContact(id);
-      console.log(removeContact);
+      console.table(removeContact);
       break;
 
     default:
